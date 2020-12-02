5c9886212ed3c373c1e7d1637360caf3cc952488
pincher <pinchers17@mail.ru>
2020-12-02 21:51:57 +0300

    add new functionality

--- script.js
+++ script.js
@@ -9,33 +9,35 @@
         }); */
     
         let param1;
+        let bitc = document.getElementById('bitcoin');
         let date1 = document.getElementById("date1");
         let btn = document.getElementById("btn");
-    let rec = () => {
-        param1 = date1.value;
         
+        async function getResponseData() {
+        param1 = date1.value;
+        let param2 = param1;
+        if(param1 != undefined ){
+        let dataResponse = await fetch('https://api.coindesk.com/v1/bpi/historical/close.json' + '?start=' + param1 + '&end=' + param2);
+        let dataContent = await dataResponse.json();
+        console.log(dataContent);
+        bitc.insertAdjacentHTML('afterEnd', `<h2>Стоимость биткоина ${param1}: ${content.bpi.USD.symbol} ${dataContent.bpi[param1]}</h2>`);
+        
+        }
        };
-       btn.addEventListener('click', rec);
+       btn.addEventListener('click', getResponseData);
        
        
-        let paramG = '2013-09-01';
-        let param2 = paramG;
+        
         
        let response = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
-       let dataResponse = await fetch('https://api.coindesk.com/v1/bpi/historical/close.json' + '?start=' + paramG + '&end=' + param2);
       
-       let dataContent = await dataResponse.json();
        let content = await response.json();
-        console.log(content);
-        console.log(dataContent);
-       
+       console.log(content);
         
-
-        let bitc = document.getElementById('bitcoin');
         const b = `<h2>Каталог</h2>`;
         bitc.insertAdjacentHTML('afterEnd', `<h2>${content.bpi.USD.symbol} ${content.bpi.USD.rate}</h2>`);
         bitc.insertAdjacentHTML('afterEnd', `<h2>${content.time.updated}</h2>`);
-        bitc.insertAdjacentHTML('afterEnd', `<h2>${dataContent.bpi["2013-09-01"]}</h2>`);
+        
     }
     getResponse();
 
